7f4a35be106529c39429b92472bbd343e614b60a
dhzdhd <ragas5432100@gmail.com>
2022-10-24 20:32:24 +0530

    Remove astro frontend and complete get article endpoint

--- backend/src/models/article.rs
+++ backend/src/models/article.rs
@@ -1,18 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct Article<'a> {
-    title: &'a str,
-    content: &'a str,
-    author: Vec<&'a str>,
+pub struct Article {
+    title: String,
+    content: String,
 }
 
-impl<'a> Article<'a> {
-    pub fn new(title: &'a str, content: &'a str, author: Vec<&'a str>) -> Self {
-        Self {
-            title,
-            content,
-            author,
-        }
+impl Article {
+    pub fn new(title: String, content: String) -> Self {
+        Self { title, content }
     }
 }
--- backend/src/routes/articles.rs
+++ backend/src/routes/articles.rs
@@ -1,18 +1,25 @@
 use crate::{database::articles::Articles, models::article::Article};
 use rocket::serde::json::Json;
+use rocket_db_pools::sqlx::Row;
 use rocket_db_pools::{sqlx::query, Connection};
 
 #[get("/articles")]
-pub fn get_all_articles<'a>() -> Json<Vec<Article<'a>>> {
-    return Json(vec![Article::new("", "", vec![""])]);
+pub fn get_all_articles<'a>() -> Json<Vec<Article>> {
+    return Json(vec![Article::new("".to_string(), "".to_string())]);
 }
 
 #[get("/articles/<id>")]
-pub fn get_one_article<'a>(mut db: Connection<Articles>, id: &str) -> Json<Article<'a>> {
-    query("SELECT ").bind(id);
-
-    return Json(Article::new("", "", vec![""]));
+pub async fn get_one_article<'a>(mut db: Connection<Articles>, id: &str) -> Option<Json<Article>> {
+    let response = query("SELECT id, title, text FROM articles WHERE id=?")
+        .bind(id)
+        .fetch_one(&mut *db)
+        .await
+        .and_then(|r| Ok(Json(Article::new(r.try_get(1)?, r.try_get(2)?))))
+        .ok();
+    response
 }
 
 #[post("/articles", format = "json", data = "<article>")]
-pub fn post_one_article(mut db: Connection<Articles>, article: Json<Article>) {}
+pub fn post_one_article(mut db: Connection<Articles>, article: Json<Article>) -> &'static str {
+    ""
+}
